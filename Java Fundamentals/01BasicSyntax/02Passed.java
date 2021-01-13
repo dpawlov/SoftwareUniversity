e0afd1a74f14fc4c11828fbb674edbd141afbe1e
Dimitar <76619952+dpawlov@users.noreply.github.com>
2021-01-13 19:36:56 +0200

    Create 02Passed.java

---
+++ Java Fundamentals/01BasicSyntax/02Passed.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.00) {
+            System.out.println("Passed!");
+        }
+    }
+    }
