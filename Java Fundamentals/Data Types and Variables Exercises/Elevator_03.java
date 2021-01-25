996b21fe21d1ca9e20702b842b6451139bd33c99
dpawlov <dimitarpawlovvv@gmail.com>
2021-01-25 23:04:14 +0200

    Exercises

---
+++ Java Fundamentals/Data Types and Variables Exercises/Elevator_03.java
@@ -0,0 +1,18 @@
+package DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class Elevator_03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int people = Integer.parseInt(scanner.nextLine());
+        int capacity = Integer.parseInt(scanner.nextLine());
+
+        double courses = people * 1.0 / capacity;
+
+        System.out.printf("%.0f", Math.ceil(courses));
+
+    }
+}
+
