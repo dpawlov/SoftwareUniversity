53d01fe0100034172724fe0c31f22cf7895e9a62
dpawlov <dimitarpawlovvv@gmail.com>
2021-01-26 19:46:49 +0200

    Exercise

---
+++ Java Fundamentals/Data Types and Variables Exercises/TriplesOfLatinLetters_06.java
@@ -0,0 +1,21 @@
+package DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters_06 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (char symbol1 = 'a'; symbol1 < 'a' + n ; symbol1++) {
+            for (char symbol2 = 'a'; symbol2 < 'a' + n ; symbol2++) {
+                for (char symbol3 = 'a'; symbol3 < 'a' + n ; symbol3++) {
+                    System.out.printf("%c%c%c%n", symbol1, symbol2, symbol3);
+                }
+            }
+        }
+
+    }
+}
