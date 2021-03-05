17ba5c3a96c9311b32b8f1e55c1af643626478d9
dpawlov <dimitarpawlovvv@gmail.com>
2021-03-05 23:51:29 +0200

    OOPLAB

---
+++ Java Fundamentals/Objects and Classes/RandomizeWords01.java
@@ -0,0 +1,25 @@
+package OOP;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomizeWords01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] sentence = scanner.nextLine().split(" ");
+
+        Random random = new Random();
+        for (int i = 0; i < sentence.length; i++) {
+            int randomIndex = random.nextInt(sentence.length);
+
+            String temp = sentence[i];
+            sentence[i] = sentence[randomIndex];
+            sentence[randomIndex] = temp;
+
+        }
+
+        for (String word : sentence) {
+            System.out.println(word);
+        }
+    }
+}
