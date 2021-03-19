8ceadaab3e4dc04c2385edc6cbdb43d07ba54e30
Dimitar <76619952+dpawlov@users.noreply.github.com>
2021-03-19 12:14:30 +0200

    Update LettersChangeNumbers08.java

--- Java Fundamentals/TextProcessing Exercise/LettersChangeNumbers08.java
+++ Java Fundamentals/TextProcessing Exercise/LettersChangeNumbers08.java
@@ -7,9 +7,9 @@ public class LettersChangeNumbers08 {
         Scanner scanner = new Scanner(System.in);
         double totalSum = 0;
         String input = scanner.nextLine();
-        //A12b s17G
+       
         String[] words = input.split("\\s+");
-        //["A12b", "s17G"]
+        
         for (String word : words) {
             double sumOfWord = getWordSum(word);
             totalSum += sumOfWord;
@@ -21,15 +21,14 @@ public class LettersChangeNumbers08 {
     }
 
     private static double getWordSum(String word) {
-        //"A12b"
+        
         double result = 0;
-        char firstLetter = word.charAt(0); //A
-        char secondLetter = word.charAt(word.length() - 1);//b
-        double number = Double.parseDouble(word.substring(1, word.length() - 1));//"12"
+        char firstLetter = word.charAt(0); 
+        char secondLetter = word.charAt(word.length() - 1);
+        double number = Double.parseDouble(word.substring(1, word.length() - 1));
 
-        //първата буква
         if(Character.isUpperCase(firstLetter)) {
-            //number / позиция на символа в азбука
+           
             result = number / (firstLetter - 64);
             number = result;
         } else if(Character.isLowerCase(firstLetter)) {
@@ -37,7 +36,7 @@ public class LettersChangeNumbers08 {
             number = result;
         }
 
-        //втората буква
+      
         if(Character.isUpperCase(secondLetter)) {
             result = number - (secondLetter - 64);
             number = result;
