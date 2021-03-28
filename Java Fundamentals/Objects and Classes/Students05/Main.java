1350a87ad36dd016a9553f274a682bb561641434
dpawlov <dimitarpawlovvv@gmail.com>
2021-03-28 23:03:43 +0300

    OOPLAB

---
+++ Java Fundamentals/Objects and Classes/Students05/Main.java
@@ -0,0 +1,39 @@
+package Students05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Student> students = new ArrayList<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            String [] tokens = input.split(" ");
+
+            String firstName = tokens[0];
+            String lastName = tokens[1];
+            int age = Integer.parseInt(tokens[2]);
+            String homeTown = tokens[3];
+
+            Student student = new Student(firstName, lastName, age, homeTown);
+
+            students.add(student);
+
+            input = scanner.nextLine();
+        }
+
+        String filterCity = scanner.nextLine();
+
+        for (Student student : students) {
+            if (student.getHomeTown().equals(filterCity)) {
+                System.out.printf("%s %s is %d years old.%n", student.getFirstName(), student.getLastName(), student.getAge(), student.getHomeTown());
+            }
+        }
+
+
+    }
+}
